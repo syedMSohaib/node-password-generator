da8e2e2ae57625ba3c9932342d8cef8e1ef8a51c
Syed Muhammad Sohaib <33151569+syedMSohaib@users.noreply.github.com>
2022-02-14 22:00:09 +0500

    completed test cases for simple password generator

--- src/__tests__/generator.test.ts
+++ src/__tests__/generator.test.ts
@@ -1,15 +1,68 @@
 import { SimplePasswordGenerator } from '../index';
 
-const options = {
-	uppercase: false,
-	lowercase: false,
-	numbers: false,
-	symbols: false,
-	length: 10,
-};
+test('exports a SimplePasswordGenerator constructor', () => {
+	const options = {
+		uppercase: true,
+		lowercase: false,
+		numbers: false,
+		symbols: false,
+		length: 10,
+	};
 
-test('exports a constructor', () => {
-	const genrator = new SimplePasswordGenerator(options);
+	const generator = new SimplePasswordGenerator(options);
+	expect(generator.generatePassword()).not.toBe('mystrongpassword');
+});
 
-	expect(genrator.generatePassword()).toBe('mystrongpassword');
+test('SimplePasswordGenerator : password should of length 20', () => {
+	const generator = new SimplePasswordGenerator({
+		uppercase: true,
+		length: 20,
+	});
+	const password = generator.generatePassword();
+	expect(password.length).toBe(20);
+});
+
+test('SimplePasswordGenerator : password should contain atleast one uppercase', () => {
+	const generator = new SimplePasswordGenerator({ uppercase: true });
+	const password = generator.generatePassword();
+	const regex = /^(?=.*?[A-Z])/g;
+	expect(password).toMatch(regex);
+});
+
+test('SimplePasswordGenerator : password should contain atleast one lowercase', () => {
+	const generator = new SimplePasswordGenerator({ lowercase: true });
+	const password = generator.generatePassword();
+	const regex = /^(?=.*?[a-z])/g;
+	expect(password).toMatch(regex);
+});
+
+test('SimplePasswordGenerator : password should contain atleast one number', () => {
+	const generator = new SimplePasswordGenerator({ numbers: true });
+	const password = generator.generatePassword();
+	const regex = /^(?=.*?[0-9])/g;
+	expect(password).toMatch(regex);
+});
+
+test('SimplePasswordGenerator : password should contain atleast both lowercase and uppercase', () => {
+	const generator = new SimplePasswordGenerator({
+		uppercase: true,
+		lowercase: true,
+	});
+	const password = generator.generatePassword();
+	const regex = /^(?=.*?[a-z])(?=.*?[A-Z])/g;
+	expect(password).toMatch(regex);
+});
+
+test('SimplePasswordGenerator : password should contain atleast numbers, special, lowercase and uppercase character', () => {
+	const generator = new SimplePasswordGenerator({
+		uppercase: true,
+		lowercase: true,
+		numbers: true,
+		symbols: true,
+		length: 15,
+	});
+	const password = generator.generatePassword();
+	const regex =
+		/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{15}$/g;
+	expect(password).toMatch(regex);
 });
